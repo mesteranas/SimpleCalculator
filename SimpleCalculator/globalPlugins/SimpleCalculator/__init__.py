ce1ae6a86f3c392d9fa4d17c4eb83a31076c93c4
mesteranas <135442710+mesteranas@users.noreply.github.com>
2023-11-05 18:40:23 +0200

    v0.1

---
+++ SimpleCalculator/globalPlugins/SimpleCalculator/__init__.py
@@ -0,0 +1,53 @@
+import ui
+import api
+import globalPluginHandler
+import wx
+import gui
+from keyboardHandler import KeyboardInputGesture
+from scriptHandler import script
+import addonHandler
+addonHandler.initTranslation()
+
+showDialog = None
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	@script(
+description= _("open semple calculator dialog"),
+category= _("SimpleCalculator"),
+gesture="kb:nvda+alt+s")
+	def script_start (self, gesture):
+		global showDialog
+		if not showDialog:
+			showDialog= mainDialog(gui.mainFrame)
+			showDialog.Raise()
+		else:
+			showDialog.Raise()
+
+class mainDialog(wx.Dialog):
+	def __init__(self, parent):
+		super(mainDialog, self).__init__(parent, title = _("Simple calculator"))
+		wx.StaticText(self, -1, _("Arithmetic operation"))
+		self.number1= wx.TextCtrl(self, -1)
+		self.number1.Bind(wx.EVT_TEXT, self.onr)
+		wx.StaticText(self, -1, _("result"))
+		self.re= wx.TextCtrl(self, -1,style=wx.TE_MULTILINE+wx.HSCROLL+wx.TE_READONLY)
+		self.Bind(wx.EVT_CHAR_HOOK, self.OnHook)
+		copy=wx.Button(self,-1, _("copy"))
+		self.Bind(wx.EVT_BUTTON, self.onLanguagec,copy)
+
+		self.Show()
+	def OnHook(self,event):
+		k=event.GetKeyCode()
+		if k==wx.WXK_ESCAPE:
+			self.Destroy()
+		event.Skip()
+	def onLanguagec(self, event):
+		if api.copyToClip(self.re.Value):
+			ui.message(_("copied"))
+
+	def onr(self, event):
+		self.re.Value=""
+		try:
+			num1=eval(self.number1.Value)
+			self.re.Value=str(num1)
+		except:
+			self.re.Value="error"
